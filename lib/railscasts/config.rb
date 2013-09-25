2d3f7e3dec079b82113b4a3d27ff85e28dda6536
lyfeyaj <lyfeyaj@gmail.com>
2013-09-25 18:03:29 +0800

    fix bugs and add one more config location

--- lib/railscasts/config.rb
+++ lib/railscasts/config.rb
@@ -10,27 +10,33 @@ module RailsCasts
       attr_reader :download_path
     
     def initialize
-      Logger.info 'Loading config file'
-      path = './../../config/railscasts.yml'
-      if File.exist? path
-        config = YAML.load(File.open(path))
-        if config["subscription-code"].present?
-          @subscription_code = config["subscription-code"]
-          Logger.info 'OK'
-        else
-          Logger.info 'Subscription code is not existed'
-          raise "Missing Subscription Code Error"
-        end
-        @file_type = config["file-type"].present? ? config["file-type"] : 'mp4'
-        if config["download-path"].present?
-          @download_path = config["download-path"]
-          Logger.info 'OK'
-        else
-          Logger.info 'Download path is not existed'
-          raise "Missing Download Path Error"
-        end
+      path = choose_path
+      config = YAML.load(File.open(path))
+      if config["subscription-code"].present?
+        @subscription_code = config["subscription-code"]
+        Logger.info 'OK'
       else
-        Logger.info 'No config file found on location: config/railscasts.yml'
+        Logger.info 'Subscription code is not existed'
+        raise "Missing Subscription Code Error"
+      end
+      @file_type = config["file-type"].present? ? config["file-type"] : 'mp4'
+      if config["download-path"].present?
+        @download_path = config["download-path"]
+        Logger.info 'OK'
+      else
+        Logger.info 'Download path is not existed'
+        raise "Missing Download Path Error"
+      end
+    end
+    
+    def choose_path
+      Logger.info 'Loading config file'
+      if File.exist? './../../config/railscasts.yml'
+        path = './../../config/railscasts.yml'
+      elsif File.exist? '~/.railscasts.yml'
+        path = '~/.railscasts.yml'
+      else
+        Logger.info 'No config file found on location: config/railscasts.yml or ~/.railscasts.yml'
         raise 'No File Found Error'
       end
     end
